19abf529902648f00cb2617fe9f3c146af7dd331
Erik Ivan Melo <erikivanmelog@gmail.com>
2026-01-09 08:52:45 -0300

    optimized datetime module

--- modules/datetime.h
+++ modules/datetime.h
@@ -1,83 +1,101 @@
 #ifndef DATETIME_H
 #define DATETIME_H
 
-#include <cstring>
-#include <time.h>
+#include <ctime>
 #include <array>
-#include <stdio.h>
+#include <cstring>
 #include "module.h"
-#include <fmt/format.h>
 
 class DateTimeModule : public Module {
-  private:
-  BarElement baseElement;
+private:
+    BarElement baseElement;
+    const char* dias[7] = {"dom","lun","mar","mié","jue","vie","sab"};
+    bool showHour = true;
 
-  public:
+    char contentBuf[32]{};  // buffer fijo
+    int lastSec = -1;
+    int lastMin = -1;
+    int lastHour = -1;
+    int lastMday = -1;
+    int lastWday = -1;
+    int lastMonth = -1;
+    int lastYear = -1;
 
-  DateTimeModule():
-    Module("datetime", false, 1),
-    dias({"dom", "lun", "mar", "mié", "jue", "vie", "sab"}),
-    showHour(true)
-  {
-
-    baseElement.moduleName = name;
-    baseElement.setEvent(
-      BarElement::CLICK_LEFT,
-      [this](){
-        this->showHour = !showHour;
-
-        if (this->showHour) {
-          // Mostrar hora: actualizar cada segundo
-          this->setSecondsPerUpdate(1);
-        } else {
-          // Solo fecha: actualizar cada minuto
-          this->setSecondsPerUpdate(60);
-        }
-        update();
-        renderFunction();
-      }
-    );
-
-    elements.push_back(&baseElement);
-  }
-  ~DateTimeModule() {
-  }
-
-  void update() {
-        time_t now = time(NULL);
-        struct tm *tm = localtime(&now);
-
-        if (showHour) {
-            baseElement.contentLen = snprintf(
-                baseElement.content,
-                CONTENT_MAX_LEN,
-                "%s %02d-%02d-%04d %02d:%02d:%02d",
-                dias[tm->tm_wday],
-                tm->tm_mday,
-                tm->tm_mon + 1,
-                tm->tm_year + 1900,
-                tm->tm_hour,
-                tm->tm_min,
-                tm->tm_sec
-            );
-        } else {
-            baseElement.contentLen = snprintf(
-                baseElement.content,
-                CONTENT_MAX_LEN,
-                "%s %02d-%02d-%04d",
-                dias[tm->tm_wday],
-                tm->tm_mday,
-                tm->tm_mon + 1,
-                tm->tm_year + 1900
-            );
-        }
-        baseElement.dirtyContent = true;
-        lastUpdate = time(nullptr);
-        std::cout << "the datetime was updated :D" << std::endl;
+    // Escribe un número de 2 dígitos en el buffer
+    inline void write2digits(char* dest, int val) {
+        dest[0] = '0' + val / 10;
+        dest[1] = '0' + val % 10;
     }
 
-    std::array<const char*, 7> dias;
-    bool showHour = false;
+public:
+    DateTimeModule() : Module("datetime", false, 1) {
+        baseElement.moduleName = name;
+        baseElement.setEvent(
+            BarElement::CLICK_LEFT,
+            [this]() {
+                showHour = !showHour;
+                setSecondsPerUpdate(showHour ? 1 : 60);
+                update();
+                renderFunction();
+            }
+        );
+        elements.push_back(&baseElement);
+    }
+
+    void update() override {
+        std::time_t now = std::time(nullptr);
+        std::tm tm{};
+        localtime_r(&now, &tm);  // thread-safe
+
+        // Solo actualizar si cambió algo
+        if (tm.tm_sec == lastSec && tm.tm_min == lastMin &&
+            tm.tm_hour == lastHour && tm.tm_mday == lastMday &&
+            tm.tm_wday == lastWday && tm.tm_mon == lastMonth &&
+            tm.tm_year == lastYear) {
+            return;
+        }
+
+        lastSec = tm.tm_sec;
+        lastMin = tm.tm_min;
+        lastHour = tm.tm_hour;
+        lastMday = tm.tm_mday;
+        lastWday = tm.tm_wday;
+        lastMonth = tm.tm_mon;
+        lastYear = tm.tm_year;
+
+        char* p = contentBuf;
+        const char* dayStr = dias[tm.tm_wday];
+
+        // Día
+        std::memcpy(p, dayStr, 3);
+        p += 3;
+        *p++ = ' ';
+
+        // Fecha
+        write2digits(p, tm.tm_mday); p += 2;
+        *p++ = '-';
+        write2digits(p, tm.tm_mon + 1); p += 2;
+        *p++ = '-';
+        int year = tm.tm_year + 1900;
+        *p++ = '0' + year / 1000; year %= 1000;
+        *p++ = '0' + year / 100;  year %= 100;
+        *p++ = '0' + year / 10;   year %= 10;
+        *p++ = '0' + year;
+
+        if (showHour) {
+            *p++ = ' ';
+            write2digits(p, tm.tm_hour); p += 2;
+            *p++ = ':';
+            write2digits(p, tm.tm_min); p += 2;
+            *p++ = ':';
+            write2digits(p, tm.tm_sec); p += 2;
+        }
+
+        baseElement.contentLen = p - contentBuf;
+        std::memcpy(baseElement.content, contentBuf, baseElement.contentLen);
+        baseElement.dirtyContent = true;
+        lastUpdate = now;
+    }
 };
 
 #endif // DATETIME_H
